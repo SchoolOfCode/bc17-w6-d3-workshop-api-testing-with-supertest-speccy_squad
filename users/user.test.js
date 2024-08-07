156c25970d1bb86ce6870bd797b72858a12465ef
Dionne Memie <d.memie@gmail.com>
2024-08-07 16:12:38 +0100

    it test done for ticket 4

--- users/user.test.js
+++ users/user.test.js
@@ -1,6 +1,6 @@
-import { test, expect } from "vitest";
+import { test, it, expect } from "vitest";
 import request from "supertest";
-import app from "../app.js"
+import app from "../app.js";
 
 //bare bones test
 test("GET /api/health works", () => {});
@@ -10,13 +10,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", function (req, res) {
-  res.status(200).json({ name: "john" });
+// app.get("/user", function (req, res) {
+//   res.status(200).json({ name: "john" });
+// });
+
+//   it("responds with json", async function () {
+//     const response = await request(app).get("/users").set("Accept", "application/json");
+//     expect(response.headers["Content-Type"]).toMatch(/json/);
+//     expect(response.status).toEqual(200)
+//   });
+
+it("Respond with JSON to GET request /api/health", async function () {
+  const response = await request(app).get("/api/health");
+  expect(response.headers["content-type"]).toMatch(/json/);
+  expect(response.status).toEqual(200);
 });
-
-
-  it("responds with json", async function () {
-    const response = await request(app).get("/users").set("Accept", "application/json");
-    expect(response.headers["Content-Type"]).toMatch(/json/);
-    expect(response.status).toEqual(200)
-  });
