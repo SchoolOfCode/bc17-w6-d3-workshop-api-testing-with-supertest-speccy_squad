f32f1dcef85f31396f76c97412905202059b21af
habib <habibali15@hotmail.com>
2024-08-07 15:54:05 +0100

    starting ticket 4

--- users/user.test.js
+++ users/user.test.js
@@ -1,5 +1,22 @@
-import { vitest } from "vitest";
 import { test, expect } from "vitest";
+import request from "supertest";
+import app from "../app.js"
 
 //bare bones test
 test("GET /api/health works", () => {});
+
+const request = require("supertest");
+const express = require("express");
+
+const app = express();
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
+});
+
+
+  it("responds with json", async function () {
+    const response = await request(app).get("/users").set("Accept", "application/json");
+    expect(response.headers["Content-Type"]).toMatch(/json/);
+    expect(response.status).toEqual(200)
+  });
